9b832ff05a9fbbbebe867bdb9fd9a936ffb5e83c
Apowalowski <acpowalowski@gmail.com>
2021-04-16 15:57:14 -0400

    AbstractClassesDemo

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class  Vehicle
+    {
+        public Vehicle()
+        {
+
+        }
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "default make";
+        public string Model { get; set; } = "default model";
+        public abstract void DriveAbstract(); //stubbed out method
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"this {GetType().Name} is virtually in drive.");
+        }
+
+    }
+}
